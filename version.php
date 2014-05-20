4dc383b42d85295c7fcd6bb0d8c20bc8111c60a1
Cathal O'Riordan <coriordan@wit.ie>
2014-05-20 12:04:40 +0100

    bumping up version number

--- version.php
+++ version.php
@@ -24,6 +24,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2013051400;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2014052000;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2012112900;        // Requires this Moodle version
 $plugin->component = 'block_academic_year'; // Full name of the plugin (used for diagnostics)
